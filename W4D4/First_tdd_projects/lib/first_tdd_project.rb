399a0ff191f712ebc54106c0f3614bfdacad0de1
lbrilli <lbrilli11@gmail.com>
2021-06-03 15:40:54 -0600

    finished two_sum and started my_transpose

--- W4D4/First_tdd_projects/lib/first_tdd_project.rb
+++ W4D4/First_tdd_projects/lib/first_tdd_project.rb
@@ -9,6 +9,18 @@ class Array
         end
         final
     end
+
+    def two_sum
+        final = []
+        self.each_with_index do |el1, idx1|
+            self.each_with_index do |el2, idx2|
+                if idx2 > idx1 && el1 + el2 == 0
+                    final << [idx1, idx2]
+                end
+            end
+        end
+        final
+    end
     
 end
 
--- W4D4/First_tdd_projects/spec/first_tdd_project_spec.rb
+++ W4D4/First_tdd_projects/spec/first_tdd_project_spec.rb
@@ -21,5 +21,35 @@ end
 
 describe "#two_sum" do
     subject(:arr) {[-1, 0, 2, -2, 1]}
-    context "finds pairs of positions that sum to zero"
+    context "finds pairs of positions that sum to zero" do
+
+        it "will return a new 2D array of index pairs" do
+            expect(arr.two_sum).to eq([[0, 4], [2, 3]])
+        end
+
+        it "should return a new array" do
+            expect(arr.two_sum).to_not eq(arr)
+        end
+
+        it "should return index from smallest to largest index" do
+            expect(arr.two_sum).to_not eq([[2, 3], [0, 4]])
+        end
+    end
+end
+
+describe "#my_transpose" do
+    subject(:arr) {[
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8]
+  ]}
+    context "will convert row-oriented array to column-oriented array" do
+
+        it "will return a new 2D array" do 
+            expect(arr.my_transpose).to_not eq(arr)
+        end
+
+        it "will return "
+
+    end
 end
