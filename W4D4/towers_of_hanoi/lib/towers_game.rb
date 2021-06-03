479a2c2c97e0b56de691e4502ba5f4de0129adb8
erikingebretson <erikingebretson@gmail.com>
2021-06-03 16:19:25 -0700

    working on move spec

---
+++ W4D4/towers_of_hanoi/lib/towers_game.rb
@@ -0,0 +1,6 @@
+# [
+#     [1,2,3,4]
+#     [nil, nil, nil, nil]
+#     [nil, nil, nil, nil]
+# ]
+
--- W4D4/towers_of_hanoi/spec/towers_game_spec.rb
+++ W4D4/towers_of_hanoi/spec/towers_game_spec.rb
@@ -1,2 +1,18 @@
 require 'rspec'
 require 'towers_game'
+
+describe "#move" do
+    subject(:arr) { [
+    [1,2,3,4],
+    [nil, nil, nil, nil],
+    [nil, nil, nil, nil] 
+    ] }
+    let(:move) { [1,4] }
+    context "moves one block(number) into another array"
+
+    it "moves to a space that was nil" do
+        expect(arr.move).to eq(nil)
+    end
+
+
+end
