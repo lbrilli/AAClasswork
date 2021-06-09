3825ee45f8431ba17a4cf442996518efe24879c4
user <user@pop-os.localdomain>
2021-06-09 11:07:27 -0700

    worked on replies table

--- W5D3/import_db.sql
+++ W5D3/import_db.sql
@@ -1,5 +1,3 @@
-
-
 CREATE TABLE users (
     id INTEGER PRIMARY KEY,
     fname TEXT NOT NULL,
@@ -7,13 +5,31 @@ CREATE TABLE users (
 );
 
 CREATE TABLE questions (
-    title TEXT,
-    body TEXT,
+    id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL,
+    body TEXT NOT NULL,
     user_id INTEGER NOT NULL,
     
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 CREATE TABLE questions_follows (
-    
+    id INTEGER PRIMARY KEY,
+    users_id INTEGER NOT NULL,
+    questions_id INTEGER NOT NULL,
+
+    FOREIGN KEY(users_id) REFERENCES users(id),
+    FOREIGN KEY(questions_id) REFERENCES questions(id)
 );
+
+CREATE TABLE replies (
+    id INTEGER PRIMARY KEY,
+    questions_id INTEGER NOT NULL,
+    users_id INTEGER NOT NULL,
+    parent_reply_id INTEGER,
+    body TEXT NOT NULL,
+
+    FOREIGN KEY(questions_id) REFERENCES questions(id),
+    FOREIGN KEY(parent_reply_id) REFERENCES replies(id),
+    FOREIGN KEY(users_id) REFERENCES users(id)
+)
