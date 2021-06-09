69e9b719f55cd907d4f1592dbff28021c9bd8054
user <user@pop-os.localdomain>
2021-06-09 14:38:03 -0700

    tested some stuff

--- W5D3/questions_database.rb
+++ W5D3/questions_database.rb
@@ -13,7 +13,7 @@ class QuestionsDatabase < SQLite3::Database
 end
 
 class User
-
+    attr_accessor :id, :fname, :lname
     def self.find_by_id(id)
         data = QuestionsDatabase.instance.execute("SELECT * FROM users WHERE id = #{id}")
         User.new(data.first)
@@ -28,37 +28,63 @@ class User
 end
 
 class Question
-
+    attr_accessor :id, :title, :body, :user_id
     def self.find_by_id(id)
         data = QuestionsDatabase.instance.execute("SELECT * FROM questions WHERE id = #{id}")
         Question.new(data.first)
     end
     
+    def initialize(options)
+        @id = options['id']
+        @title = options['title']
+        @body = options['body']
+        @user_id = options['user_id']
+    end
+
 end
 
 class QuestionFollow
-
+    attr_accessor :id, :users_id, :questions_id
     def self.find_by_id(id)
         data = QuestionsDatabase.instance.execute("SELECT * FROM questions_follows WHERE id = #{id}")
         QuestionFollow.new(data.first)
     end
     
+    def initialize(options)
+        @id = options['id']
+        @users_id = options['users_id']
+        @questions_id = options['questions_id']
+    end
 end
 
 class Reply
-
+    attr_accessor :id, :parent_reply_id, :users_id, :questions_id, :body
     def self.find_by_id(id)
         data = QuestionsDatabase.instance.execute("SELECT * FROM replies WHERE id = #{id}")
         Reply.new(data.first)
+    end
+
+    def initialize(options)
+        @id = options['id']
+        @parent_reply_id = options['parent_reply_id']
+        @users_id = options['users_id']
+        @questions_id = options['questions_id']
+        @body = options['body']
     end
     
 end
 
 class QuestionLike
-
+    attr_accessor :id, :users_id, :questions_id
     def self.find_by_id(id)
         data = QuestionsDatabase.instance.execute("SELECT * FROM question_likes WHERE id = #{id}")
         QuestionLike.new(data.first)
+    end
+
+    def initialize(options)
+        @id = options['id']
+        @users_id = options['users_id']
+        @questions_id = options['questions_id']
     end
 
 end
