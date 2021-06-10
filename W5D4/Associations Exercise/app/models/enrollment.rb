6eb44db9237594a6cf23af3b572cd21d0556c22d
Jay Lam <lamjay415@gmail.com>
2021-06-10 15:38:50 -0700

    completed association exercise

--- W5D4/Associations Exercise/app/models/enrollment.rb
+++ W5D4/Associations Exercise/app/models/enrollment.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: enrollments
+#
+#  id         :bigint           not null, primary key
+#  course_id  :integer
+#  student_id :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Enrollment < ApplicationRecord
     belongs_to :user,
         primary_key: :id,
@@ -9,3 +19,4 @@ class Enrollment < ApplicationRecord
         foreign_key: :course_id,
         class_name: :Course
 end
+
