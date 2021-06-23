3bf04e6ebc66613c8abdf383f8645c22e7c259cd
b-chai <b.chai82@gmail.com>
2021-06-23 17:17:54 -0400

    added more specs

--- W7D3/goal_app/spec/models/user_spec.rb
+++ W7D3/goal_app/spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:incomplete_user) {User.new(username: "", password: "hunter12")}
+  # subject(:user) {User.new(username: "aaTester", password: "hunter12")}
   
   it {should validate_presence_of(:username)}
   it {should validate_presence_of(:password_digest)}
@@ -10,6 +10,33 @@ RSpec.describe User, type: :model do
   it {should validate_uniqueness_of(:username)}
   it {should validate_uniqueness_of(:session_token)}
 
-  
+  it {should have_many(:goals)}
+  it {should have_many(:comments)}
 
+  describe 'is_password' do
+    let!(:user) {build(:user)}
+
+    context "with a valid password" do
+      it "should return true" do
+        expect (user.is_password("hunter12")).to be true
+      end
+    end
+
+    context "with an invalid password" do
+      it "should return false" do
+        expect (user.is_password("banana")).to be false
+      end
+    end
+  end
+
+  describe "::find_by_credential" do
+    before {user.save}
+    it "returns a user if credentials match" do
+      expect(User.find_by_credential("aaTester","hunter12")).to eq(user)
+    end
+
+    it "returns nil if no match is found" do
+      expect(User.find_by_credential("aaTester","1234567")).to eq(nil)
+    end
+  end
 end
