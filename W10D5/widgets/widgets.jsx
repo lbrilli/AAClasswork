004ccac4f894a2281fe8dba68732635a4c95c024
lbrilli <lbrilli11@gmail.com>
2021-07-16 15:57:16 -0600

    setting up initial files

---
+++ W10D5/widgets/widgets.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener("DOMContentLoaded", event => {
+    const root = document.getElementById("root");
+
+    ReactDOM.render(<App name="Widgets" />, root);
+});
